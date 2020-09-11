3feef354484540558b6a70dcdc63a8bdabf15c4c
FoPQer <iritskiy.sas@gmail.com>
2020-09-11 15:12:33 +0300

    Доработка 2.1

--- src/Prac2/Number1/Main.java
+++ src/Prac2/Number1/Main.java
@@ -1,14 +1,38 @@
 package Prac2.Number1;
 
+import java.util.Scanner;
+
 public class Main
 {
     public static void main(String[] args)
     {
+        Scanner sc = new Scanner(System.in);
         boolean more = true;
+        int sup, i = 0;
+        Shape[] shapes = new Shape[10];
         while (more)
         {
             Shape shape = new Shape();
-            System.out.println("Еще фигуру?");
+            shapes[i] = shape;
+            i++;
+            System.out.println("Еще фигуру?(1 да, 0 нет)");
+            sup = sc.nextInt();
+            switch (sup) {
+                case 0:
+                    more = false;
+                    break;
+                case 1:
+                    more = true;
+                    break;
+                default:
+                    System.out.println("Введено не верное значение, будем считать, что мы закончили");
+                    more = false;
+                    break;
+            }
+        }
+        for (int j = 0; j < i; j++)
+        {
+            shapes[j].out();
         }
     }
 }
--- src/Prac2/Number1/Shape.java
+++ src/Prac2/Number1/Shape.java
@@ -4,17 +4,24 @@ import java.util.Scanner;
 
 public class Shape
 {
+    String name;
     int stor;
     double volume;
     double square;
     Shape()
     {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Введите название фигуры");
+        name = sc.nextLine();
         System.out.println("Введите количество сторон");
         stor = sc.nextInt();
         System.out.println("Введите площадь поверхности");
         square = sc.nextDouble();
-        System.out.println("Введите объем");
+        System.out.println("Введите объём");
         volume = sc.nextDouble();
+    }
+    void out()
+    {
+        System.out.println("Название: " + name + " количество сторон: " + stor + " площадь поверхности: " + square + " объём " + volume);
     }
 }
