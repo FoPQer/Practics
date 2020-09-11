ec5f5dc6c0f9846874b9d9c8860f5a399ca257fe
FoPQer <iritskiy.sas@gmail.com>
2020-09-11 11:56:36 +0300

    Add practics 1.3 - 1.5

---
+++ src/Prac2/Number1/Main.java
@@ -0,0 +1,14 @@
+package Prac2.Number1;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        boolean more = true;
+        while (more)
+        {
+            Shape shape = new Shape();
+            System.out.println("Еще фигуру?");
+        }
+    }
+}
