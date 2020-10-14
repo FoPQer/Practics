1a1c4b15f6e1ea991544cae4f1529ad03e88eb14
FoPer <iritskiy.sas@gmail.com>
2020-10-15 00:34:27 +0300

    Наметки 8.2

--- src/Prac8/Number2/Picture.java
+++ src/Prac8/Number2/Picture.java
@@ -2,22 +2,26 @@ package Prac8.Number2;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
-public class Picture extends JFrame
-{
-    Picture()
-    {
-        JFrame frm = new JFrame("Картинка");
-        frm.setBounds(300,300,400,400);
-        frm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frm.setLayout(null);
-        ImageIcon image = new ImageIcon("")
-        frm.setVisible(true);
+public class Picture extends  JFrame {
+    public static void main (String[] args) {
+        Picture main = new Picture();
+        main.go();
     }
 
-    public static void main(String[] args) {
-        Picture pic = new Picture();
+    public void go () {
+        JFrame frame = new JFrame();
+        MyDrawPanel panel = new MyDrawPanel();
+
+        frame.getContentPane().add(panel);
+
+        frame.setSize(300, 300);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
+class MyDrawPanel extends JPanel {
+    public void paintComponent (Graphics g) {
+        Image image = new ImageIcon("D:\\Picture").getImage();
+        g.drawImage(image, 3, 4, this);
     }
 }
